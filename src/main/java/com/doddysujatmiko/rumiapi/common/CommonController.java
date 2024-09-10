1024a0d35bfc73464639aa73ac164c0cccd7a97f
doddy-s <doddy.s@students.amikom.ac.id>
2024-09-10 08:31:16 +0700

    feat: simple welcome page on root endpoint

--- src/main/java/com/doddysujatmiko/rumiapi/common/CommonController.java
+++ src/main/java/com/doddysujatmiko/rumiapi/common/CommonController.java
@@ -1,13 +1,28 @@
 package com.doddysujatmiko.rumiapi.common;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.function.BiFunction;
+
 @RestController
-@RequestMapping("/common")
+@RequestMapping("/")
 public class CommonController {
+    @Value("${RUMI_API_HOST}")
+    private String rumiApiHost;
+
+    @GetMapping("/")
+    String root() {
+        BiFunction<String, String, String> createAnchor = (href, text) -> "<a href=\""+href+"\"> "+text+" </a>";
+        return "<!DOCTYPE html> Welcome to RumiApi. Click " +
+                createAnchor.apply(rumiApiHost+"/swagger-ui/index.html", "this link") +
+                " to open documentation. Or " +
+                createAnchor.apply("http://localhost:8080/swagger-ui/index.html", "this link") +
+                "if you run this api in localhost.";
+    }
+
     @GetMapping("/hello")
     String getHello() {
         return "Hello";
